2906bc10be60f9882f115e6614437e15209e9c51
bpinheiro <bernardopinheiro19@gmail.com>
2023-09-20 21:19:03 +0100

    Implemented piece movement

--- py2048.py
+++ py2048.py
@@ -9,7 +9,7 @@ class App:
         self.screen = None
         self.clock = pygame.time.Clock()
         self.width, self.height = 800, 800
-        self.board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        self.board = [[2, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         self.screen_color = pygame.Color(207, 195, 176)
 
     def start_game(self):
@@ -28,7 +28,17 @@ class App:
         pygame.quit()
 
     def draw_board(self):
-        pass
+        x,y = 100,100
+        for i in range (3):
+            x += 150##height / 4
+            y += 150##width / 4
+            pygame.draw.line(self.screen, (0,0,0), (x,100), (x, self.height-100), width=3)
+            pygame.draw.line(self.screen, (0,0,0), (100,y), (self.width-100, y), width=3)
+
+        pygame.draw.line(self.screen, (0,0,0), (100,100), (100, self.height-100), width=3)
+        pygame.draw.line(self.screen, (0,0,0), (100,self.height-100), (self.width-100, self.height-100), width=3)
+        pygame.draw.line(self.screen, (0,0,0), (self.width-100, self.height-100), (self.width-100, 100), width=3)
+        pygame.draw.line(self.screen, (0,0,0), (100,100), (self.width-100, 100), width=3)
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -46,6 +56,74 @@ class App:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             self.move_right()
 
+    def move_up(self):
+        for col in range(4):
+            row = 0
+            while row < 4:
+                current = self.board[row][col]
+                if current != 0 and row-1 >= 0:
+                    if not self.piece_has_value(row-1, col):
+                        self.move_piece(row, col, row-1, col)
+                        row-=2
+                    else:
+                        if self.board[row][col] == self.board[row-1][col]:
+                            self.board[row-1][col] = 2 * self.board[row][col]
+                            self.board[row][col] = 0
+                row+=1   
+    
+    def move_down(self):
+        for col in range(4):
+            row = 3
+            while row >= 0:
+                current = self.board[row][col]
+                if current != 0 and row+1 < 4:
+                    if not self.piece_has_value(row+1, col):
+                        self.move_piece(row, col, row+1, col)
+                        row+=2
+                    else:
+                        if self.board[row][col] == self.board[row+1][col]:
+                            self.board[row+1][col] = 2 * self.board[row][col]
+                            self.board[row][col] = 0
+                row-=1             
+ 
+    def move_left(self):
+        for row in range(4):
+            col = 0
+            while col < 4:
+                current = self.board[row][col]
+                if current != 0 and col-1 >= 0:
+                    if not self.piece_has_value(row, col-1):
+                        self.move_piece(row, col, row, col-1)
+                        col-=2
+                    else:
+                        if self.board[row][col] == self.board[row][col-1]:
+                            self.board[row][col-1] = 2 * self.board[row][col]
+                            self.board[row][col] = 0
+                col+=1
+
+    def move_right(self):
+        for row in range(4):
+            col = 3
+            while col >= 0:
+                current = self.board[row][col]
+                if current != 0 and col+1 < 4:
+                    if not self.piece_has_value(row, col+1):
+                        self.move_piece(row, col, row, col+1)
+                        col+=2
+                    else:
+                        if self.board[row][col] == self.board[row][col+1]:
+                            self.board[row][col+1] = 2 * self.board[row][col]
+                            self.board[row][col] = 0
+                col-=1  
+
+    def piece_has_value(self, x, y):
+        return self.board[x][y] != 0
+    
+    def move_piece(self, x, y, z, w):
+        val = self.board[x][y]
+        self.board[x][y] = 0
+        self.board[z][w] = val
+
     def update(self):
         pass
 
@@ -55,10 +133,15 @@ class App:
     def spawn_tile(self):
         pass
 
+    def print_board(self):
+        for row in range(4):
+            print(self.board[row])
+
     def main_loop(self):
         while self.running:
             for event in pygame.event.get():
                 self.on_event(event)
+                self.print_board()
 
             self.update()
             self.render()
